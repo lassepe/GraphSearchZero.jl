7ba643694e3bf706d7aad1ada876116711f83471
Lasse Peters <lasse.peters@tuhh.de>
2019-04-30 15:14:21 -0700

    Update GraphSearchLight.jl

--- src/GraphSearchLight.jl
+++ src/GraphSearchLight.jl
@@ -5,6 +5,7 @@ export SearchProblem
 # Describes how problems need to be specified in order to work with this module.
 include("problem_interface.jl")
 # A simple but fairly fast implementation of a search node.
+export SearchNode
 include("search_node.jl")
 # Implementations of some graphs search algorithms.
 export
